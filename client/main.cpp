da6308827450cc6ae4e12961cc933ebbd53db6a1
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2018-01-12 16:04:14 +0300

    Добавлен компилируемы thrift-файл, его компиляция и тест.

--- client/main.cpp
+++ client/main.cpp
@@ -2,13 +2,20 @@
 
 #include <thrift/protocol/TBase64Utils.h>
 #include <thrift/protocol/TBinaryProtocol.h>
+#include "thrifts_types.h"
 
 int main(int argc, char* argv[])
 {
+    thrifts::SharedStruct s1;
+    s1.__set_key(1);
+    s1.__set_value("test");
+    thrifts::SharedStruct s2(s1);
+
     char buf1[10] = "\0\0\0\0\0";
     char buf2[10] = "_________";
     apache::thrift::protocol::base64_encode((uint8_t*)buf1, 3, (uint8_t*)buf2);
 
     printf("base64_encode result: %s\n", buf2);
+    printf("s2.value: \"%s\"\n", s2.value.c_str());
     return 0;
 }
---
+++ thrifts/thrifts.thrift
@@ -0,0 +1,14 @@
+
+namespace cpp thrifts
+namespace java thrifts
+
+struct SharedStruct {
+  1: i64 key
+  2: string value
+}
+
+service SharedService {
+  bool putPair(1: i64 key, 2: string value);
+  void replacePair(1: i64 key, 2: string value);
+  SharedStruct getStruct(1: i64 key)
+}
