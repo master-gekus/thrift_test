fdbb3c2ee507e56065897be6f2e929dd791a0849
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2018-01-12 06:53:07 +0300

    Предварительная версия внешнего проекта для Thrift (MSVC)

---
+++ client/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <cstdio>
+
+#include <thrift/protocol/TBase64Utils.h>
+#include <thrift/protocol/TBinaryProtocol.h>
+
+int main(int argc, char* argv[])
+{
+    char buf1[10] = "\0\0\0\0\0";
+    char buf2[10] = "_________";
+    apache::thrift::protocol::base64_encode((uint8_t*)buf1, 3, (uint8_t*)buf2);
+
+    printf("base64_encode result: %s\n", buf2);
+    return 0;
+}
